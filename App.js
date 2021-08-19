14cc79720cb3c558e430eb57feb708de42cdfd33
ANDRÉ MYSZKO <andre.myszko@gmail.com>
2021-08-19 17:28:44 -0300

    update. switch frontal/back camera

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, {useState, useEffect} from 'react';
-import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView, TouchableOpacity } from 'react-native';
 import {Camera} from 'expo-camera';
 
 export default function App() {
@@ -26,7 +26,27 @@ export default function App() {
       <Camera 
         style={{ flex: 1 }}
         type={type}
-      />
+      >
+        <View style={{flex: 1, backgroundColor: 'Transparent', flexDirection: 'row'}}>
+          <TouchableOpacity
+            style={{
+              position: 'absolute',
+              bottom: 20,
+              left: 20,
+            }}
+            onPress={ () => {
+              setType(
+                type === Camera.Constants.Type.back
+                ? Camera.Constants.Type.front
+                : Camera.Constants.Type.back
+              );
+            }}
+          >
+            <Text style={{ fontSize: 20, marginBottom: 13, color: '#FFF'}}>Switch Camera</Text>
+          </TouchableOpacity>
+        </View>
+      </Camera>
+      
     </SafeAreaView>
   );
 }
